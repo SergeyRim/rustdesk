3f9ea2ee4f73feea382c8a156618b615fa8e7c49
SergeyRim <derek@westnet.by>
2024-07-14 10:55:13 +0300

    Update it.rs

--- src/lang/it.rs
+++ src/lang/it.rs
@@ -2,7 +2,7 @@ lazy_static::lazy_static! {
 pub static ref T: std::collections::HashMap<&'static str, &'static str> =
     [
         ("Status", "Stato"),
-        ("Your Desktop", "Questo desktop"),
+        ("Your Desktop", "Questo desktop ip4.by"),
         ("desk_tip", "Puoi accedere a questo desktop usando l'ID e la password indicati qui sotto."),
         ("Password", "Password"),
         ("Ready", "Pronto"),
